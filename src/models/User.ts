f8fc0c04dd8ca092aa78be87688805d00a394675
Chris Patrick <atomize101@gmail.com>
2021-02-13 11:11:32 -0500

    Storing event listeners

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,15 @@
 interface UserProps {
-	name: string;
-	age: number;
+	name?: string;
+	age?: number;
 }
 
+type Callback = () => {};
+
 export class User {
+	// This tells typescript that the events will be a string of type Callback that is an array.
+	// We use this type of annotation to store Event listeners
+	events: { [key: string]: Callback[] } = {};
+
 	constructor(private data: UserProps) {}
 
 	get(propName: string): number | string {
@@ -13,4 +19,6 @@ export class User {
 	set(update: UserProps): void {
 		Object.assign(this.data, update);
 	}
+
+	on(eventName: string, callback: Callback) {}
 }
