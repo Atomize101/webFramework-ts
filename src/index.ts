5d5dfe34ab558ac36b7d6483bc7ef5b8b35bffa7
Chris Patrick <atomize101@gmail.com>
2021-02-14 11:48:30 -0500

    Creating the fetch method and wiring with axios

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-	console.log('Change #1');
-});
-user.on('change', () => {
-	console.log('Change #2');
-});
-user.on('save', () => {
-	console.log('Save Triggered');
-});
+user.fetch();
 
-user.trigger('change');
-user.trigger('save');
+setTimeout(() => {
+	console.log(user);
+}, 4000);
