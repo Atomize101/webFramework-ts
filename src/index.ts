3aafe4bb6b86101b7c6796c3e7468fb487a0dbb6
Chris Patrick <atomize101@gmail.com>
2021-02-14 15:09:14 -0500

    Composition with nested objects

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'New Record', age: 999 });
 
-user.save();
+user.events.on('change', () => {
+	console.log('Change!');
+});
+
+user.events.trigger('change');
