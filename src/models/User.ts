234a2450e3467ae9300aa434cc23d7bf30b22f4e
Chris Patrick <atomize101@gmail.com>
2021-02-16 14:44:18 -0500

    Refactor sync

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
 	id?: number;
@@ -8,6 +9,7 @@ export interface UserProps {
 
 export class User {
 	public events: Eventing = new Eventing();
+	public sync: Sync<UserProps> = newSync<UserProps>();
 
 	constructor(private data: UserProps) {}
 
